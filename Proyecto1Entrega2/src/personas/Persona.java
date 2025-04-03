27f25df16b7218a89703236e8a0e6ba7abcb51a3
julianramosss04 <jd.ramosg1@uniandes.edu.co>
2025-04-02 20:21:33 -0500

    paquete personas

--- Proyecto1Entrega2/src/personas/Persona.java
+++ Proyecto1Entrega2/src/personas/Persona.java
@@ -1,5 +1,12 @@
 package personas;
 
+import java.util.List;
+
 public abstract class Persona {
+	
+	protected String nombre;
+    protected String login;
+    protected String password;
+    protected List<String> enfermedadesDiscapacidades;
 
 }
